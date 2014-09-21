e093dfefd17bf0738b5de826a905d79c14c75b40
aleung013 <aleung013@gmail.com>
2014-09-21 12:55:25 -0400

    Start work on parse function

--- client.py
+++ client.py
@@ -1,4 +1,16 @@
 import socket, select, string, sys
+from communistpoker import COMBOS
+from deck import VALUES,SUITS
+def parseCombo(msg):
+    combo = []
+    for i in range(len(COMBOS)):
+        if(msg.find(COMBOS[i])):
+            combo[0] = COMBOS[i]
+    if(combo[1] == COMBOS[3]):#Flush
+        for i in range(len(SUITS)):
+            if(msg.find(SUITS[i])):
+                combo[1] = COMBOS[i]
+    
 
 def prompt():
     sys.stdout.write('<You> ')
@@ -43,6 +55,13 @@ if __name__ == "__main__":
                         data = sock.recv(4096)
                         sys.stdout.write("Cards received: \n")
                         sys.stdout.write(data)
+                    elif (data == "play_round"):
+                        data = sock.recv(4096) #get the old combo
+                        sys.stdout.write("What do you call?\n")
+                        msg = sys.stdin.readline()
+                        combo = parseCombo(msg)
+                        s.send(combo)
+                        
                     else:
                         sys.stdout.write(data)
                 else:
